881427b647bf04d05cff13ba9d33fb92409544f9
Elwing-Chou <birdfan8814@gmail.com>
2026-08-17 14:21:22 +0800

    Add files via upload

--- 2_剪刀石頭布.py
+++ 2_剪刀石頭布.py
@@ -12,3 +12,11 @@ p2 = random.randint(0, 2)
 names = ["剪刀", "石頭", "布"]
 print("我出:" + names[p1])
 print("電腦出:" + names[p2])
+
+# %: mod
+if p1 == (p2 + 1) % 3:
+    print("p1 win")
+elif p2 == (p1 + 1) % 3:
+    print("p2 win")
+else:
+    print("even")
