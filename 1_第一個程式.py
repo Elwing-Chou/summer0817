109fd502c5d40b3f8209d8088a6e665936db1ad5
Elwing-Chou <birdfan8814@gmail.com>
2026-08-17 11:00:30 +0800

    Add files via upload

--- 1_第一個程式.py
+++ 1_第一個程式.py
@@ -1,1 +1,15 @@
+# 型態: 分類
+# 數字分類(int整數/float小數)  文字分類(str)
 print(30)
+print("apple")
+# 如果...否則: if else
+# =:
+# 5 == 2 + 3 相等?
+# x = 3 是/給個符號
+# = 左右兩邊空白建是排版
+# if else:強制排版(縮排)
+age = 16
+if age >= 18:
+    print("成年")
+else:
+    print("未成年")
