11048b388b8ed0910b811ce875552221a8da940b
Elwing-Chou <birdfan8814@gmail.com>
2026-08-17 15:20:12 +0800

    Add files via upload

--- 2_剪刀石頭布.py
+++ 2_剪刀石頭布.py
@@ -1,4 +1,5 @@
 # 引用別的.py
+import os
 import random
 import pandas as pd
 import datetime as dt
@@ -17,12 +18,35 @@ print("電腦出:" + names[p2])
 
 # %: mod
 if p1 == (p2 + 1) % 3:
+    result = "win"
     print("p1 win")
 elif p2 == (p1 + 1) % 3:
+    result = "lose"
     print("p2 win")
 else:
+    result = "even"
     print("even")
 
 # CSV格式(Comma-Separated Values)
 # 姓名,身高
 # Elwing,175
+# 如果沒有這個檔案
+if not os.path.exists("record.csv"):
+    # pandas的表格我們叫他DataFrame(自創型態)
+    # 我們就創一個新的表格
+    df = pd.DataFrame(columns=["time", "p1", "p2", "result"])
+else:
+    # 我就讀取舊的表格
+    df = pd.read_csv("record.csv")
+
+# 如果你的編號是你自訂一(time/p1/p2...): 字典{}
+row = {
+    "time":str(dt.datetime.now()),
+    "p1":names[p1],
+    "p2":names[p2],
+    "result":result
+}
+# 這個以後再說:.loc拿列 把最新一列設成這個東西
+# pandas列型態: Series
+df.loc[len(df)] = pd.Series(row)
+df.to_csv("record.csv", index=False)
