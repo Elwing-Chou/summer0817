59b8e8f3e5bef3fd568cc54f6bf19de24a3b5968
Elwing-Chou <birdfan8814@gmail.com>
2026-08-19 14:05:10 +0800

    Add files via upload

--- 6_baha.py
+++ 6_baha.py
@@ -1,5 +1,6 @@
 import urllib.request as req
 import json
+import pandas as pd
 
 url = "https://api.gamer.com.tw/anime/v1/danmu.php?videoSn=36632&geo=TW%2CHK"
 f = req.urlopen(url)
@@ -7,4 +8,6 @@ s = f.read()
 # [] {} -> JSON格式
 s = json.loads(s)
 danmus = s["data"]["danmu"]
-print(danmus)
+df = pd.DataFrame(danmus)
+df.to_csv("danmu.csv", index=False)
+print(df)
