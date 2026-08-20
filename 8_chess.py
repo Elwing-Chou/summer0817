cd8ef6693dc56c350fa5c87249f204cff7ebf255
Elwing-Chou <birdfan8814@gmail.com>
2026-08-20 16:01:11 +0800

    Add files via upload

--- 8_chess.py
+++ 8_chess.py
@@ -39,8 +39,14 @@ tags = {
     0:["將", "帥"]
 }
 
+# 第幾回合
+game_round = 0
+# [i, j]
+chosen = None
 # 重新繪製整個畫布
 def refresh():
+    global chosen, game_round
+
     # 建立畫布bg
     bg = pg.Surface(screen.get_size())
     # 把畫布填滿某個顏色rgb
@@ -111,10 +117,13 @@ def refresh():
                 bg.blit(txt, txt.get_rect(center=[cx, cy]))
 
     # 畫選取框
-    pg.draw.rect(bg,
-                 COLOR_RED,
-                 [2*inter-inter/2, inter-inter/2, inter, inter],
-                 1)
+    if not chosen == None:
+        ci, cj = chosen
+        cy, cx = (ci + 1) * inter, (cj + 1) * inter
+        pg.draw.rect(bg,
+                     COLOR_RED,
+                     [cx-inter/2, cy-inter/2, inter, inter],
+                     1)
 
     screen.blit(bg, [0,0])
     # 對畫面進行更新(才會真的秀出來)
@@ -123,9 +132,6 @@ def refresh():
 
 refresh()
 # 建立一個永不結束的迴圈(遊戲才不會結束)
-# 第幾回合
-game_round = 0
-chosen = None
 running = True
 while running:
     # 收取你的遊戲任何事件(滑鼠點擊/鍵盤按鈕...)
@@ -133,6 +139,51 @@ while running:
         # 偵測滑鼠點擊以後放掉的動作
         if event.type == pg.MOUSEBUTTONUP:
             m_x, m_y = pg.mouse.get_pos()
+            print(m_x, m_y)
+            # 座標/80 -> (160, 80) -> (2, 1)  (180, 100) -> (2.25, 1.25)
+            m_x_1, m_y_1 = m_x / inter, m_y / inter
+            # (2.25, 1.25) -> (2, 1)
+            m_x_1, m_y_1 = round(m_x_1), round(m_y_1)
+            # (2, 1) -> 邏輯坐標系 [1][0]
+            ci, cj  = m_y_1 - 1, m_x_1 - 1
+            # 換成棋盤i, j
+            # 點下去
+            # 1. 之前已經選一顆了(移動or換一顆)
+            chess = chess_board[ci][cj]
+            if not chosen == None:
+                # 新選的
+                # 舊的棋子
+                lasti, lastj = chosen
+                last_chess = chess_board[lasti][lastj]
+                # case 1.1 空白位置
+                if chess == None:
+                    # 移動
+                    chess_board[ci][cj] = last_chess
+                    chess_board[lasti][lastj] = None
+                    game_round = game_round + 1
+                    chosen = None
+                # case 1.2 有棋子
+                else:
+                    # case 1.2.1 我方: 換一顆
+                    if chess[1] == last_chess[1]:
+                        chosen = [ci, cj]
+                    # case 1.2.2 敵方: 吃
+                    else:
+                        chess_board[ci][cj] = last_chess
+                        chess_board[lasti][lastj] = None
+                        game_round = game_round + 1
+                        chosen = None
+            # 2. 之前沒有選
+            else:
+                # 2.1 新的是空白
+                if chess == None:
+                    pass
+                else:
+                    # 2.2 新的不是空白: 選擇(依據game_round)
+                    if chess[1] == game_round % 2:
+                        chosen = [ci, cj]
+                        print(chosen)
+            refresh()
         # 如果收到的事件是按x
         if event.type == pg.QUIT:
             # 迴圈就會變成while False
