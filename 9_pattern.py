9c44c4cb7a305a34cd7a6a83893df4950531a68f
Elwing-Chou <birdfan8814@gmail.com>
2026-08-20 14:03:57 +0800

    Add files via upload

--- 9_pattern.py
+++ 9_pattern.py
@@ -6,3 +6,16 @@ print(add(5, 6))
 print(add(7, 8))
 
 print(5 < 3)
+
+test = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1],
+    [0, 0, 0]
+]
+print(test[1][2])
+
+for i in range(4):
+    print(test[i])
+    for j in range(3):
+        print(test[i][j])
